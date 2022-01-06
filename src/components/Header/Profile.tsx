4ef9f570cc097bc6e73d8cc7ed5702bfa08b3eec
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-01-06 16:19:16 -0300

    Responsive Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Rodrigo Baggio</Text>
-        <Text color="gray.300" fontSize="small">
-          rodrigo.baggio.si@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Rodrigo Baggio</Text>
+          <Text color="gray.300" fontSize="small">
+            rodrigo.baggio.si@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar
         size="md"
         name="Rodrigo Baggio"
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -2,8 +2,13 @@ import { Box, Stack } from "@chakra-ui/react";
 import {
   RiContactsLine,
   RiDashboardLine,
-  RiGitMergeLine,
+  RiMoneyDollarCircleLine,
+  RiGroupFill,
   RiInputMethodLine,
+  RiBankLine,
+  RiSafeLine,
+  RiAddBoxLine,
+  RiSubtractLine,
 } from "react-icons/ri";
 import { NavLink } from "./NavLink";
 import { NavSection } from "./NavSection";
@@ -14,11 +19,16 @@ export function Sidebar() {
       <Stack spacing="12" align="flex-start">
         <NavSection title="GERAL">
           <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
+          <NavLink icon={RiDashboardLine}>Históricos</NavLink>
+          <NavLink icon={RiGroupFill}>Pessoas</NavLink>
+          <NavLink icon={RiMoneyDollarCircleLine}>Contas</NavLink>
+          <NavLink icon={RiBankLine}>Bancos</NavLink>
           <NavLink icon={RiContactsLine}>Usuários</NavLink>
         </NavSection>
-        <NavSection title="AUTOMAÇÃO">
-          <NavLink icon={RiInputMethodLine}>Formulários</NavLink>
-          <NavLink icon={RiGitMergeLine}>Automação</NavLink>
+        <NavSection title="FINANCEIRO">
+          <NavLink icon={RiSafeLine}>Lançamentos</NavLink>
+          <NavLink icon={RiAddBoxLine}>Recebimentos</NavLink>
+          <NavLink icon={RiSubtractLine}>Pagamentos</NavLink>
         </NavSection>
       </Stack>
     </Box>
