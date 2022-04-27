0b6851a17bb33f3802cf410f84413f5d48307e08
rodrigo.baggio.si@gmail.com <Baggio25>
2022-04-27 09:49:47 -0300

    Configuring React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useEffect } from "react";
+import { useQuery } from "react-query";
 import { RiAddLine, RiDeleteBinLine, RiPencilLine } from "react-icons/ri";
 import {
   Box,
@@ -8,6 +9,7 @@ import {
   Heading,
   HStack,
   Icon,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -18,22 +20,22 @@ import {
   useBreakpointValue,
 } from "@chakra-ui/react";
 
-
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery("users", async () => {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
+
+    return data;
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
-
-  useEffect(() => {
-    fetch("http://localhost:3000/api/users")
-      .then(response => response.json())
-      .then(data => console.log(data));
-  }, []); 
 
   return (
     <Box>
@@ -60,53 +62,65 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de Cadastro</Th>}
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Rodrigo Baggio</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      rodrigo.baggio.si@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>05 de janeiro de 2022</Td>}
-                <Td>
-                  <HStack spacing="2" mx="1" pr="4" py="1">
-                    <Button
-                      as="a"
-                      size="xs"
-                      fontSize="xs"
-                      colorScheme="gray"
-                      color="gray.700"
-                      leftIcon={<Icon as={RiPencilLine} />}
-                    >
-                      {isWideVersion ? "Editar" : ""}
-                    </Button>
-                    <Button
-                      as="a"
-                      size="xs"
-                      fontSize="xs"
-                      colorScheme="red"
-                      color="gray.100"
-                      leftIcon={<Icon as={RiDeleteBinLine} />}
-                    >
-                      {isWideVersion ? "Excluir" : ""}
-                    </Button>
-                  </HStack>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de Cadastro</Th>}
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Rodrigo Baggio</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          rodrigo.baggio.si@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>05 de janeiro de 2022</Td>}
+                    <Td>
+                      <HStack spacing="2" mx="1" pr="4" py="1">
+                        <Button
+                          as="a"
+                          size="xs"
+                          fontSize="xs"
+                          colorScheme="gray"
+                          color="gray.700"
+                          leftIcon={<Icon as={RiPencilLine} />}
+                        >
+                          {isWideVersion ? "Editar" : ""}
+                        </Button>
+                        <Button
+                          as="a"
+                          size="xs"
+                          fontSize="xs"
+                          colorScheme="red"
+                          color="gray.100"
+                          leftIcon={<Icon as={RiDeleteBinLine} />}
+                        >
+                          {isWideVersion ? "Excluir" : ""}
+                        </Button>
+                      </HStack>
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
